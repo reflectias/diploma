dc452d815d686d1619bda102165b7eeba76bed00
Reflectias <vladix98@gmail.com>
2024-04-29 12:10:12 +0300

    adding files

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://reflectias:P@ssw0rd123@db:3306/database"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = User.query.all()
+    return jsonify([{"id": user.id, "name": user.name, "email": user.email} for user in users])
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    user = User(name=data["name"], email=data["email"])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"id": user.id, "name": user.name, "email": user.email})
+
+if __name__ == "__main__":
+    app.run(debug=True)
